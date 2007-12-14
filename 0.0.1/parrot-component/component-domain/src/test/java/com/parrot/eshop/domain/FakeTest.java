becce78f113ea201bd1bc6a7c32ffa76712b4de7
v.tajzich <v.tajzich@9f38408c-3b41-0410-94e9-c98551d686dc>
2007-12-14 09:33:47 +0000

    Base folder structure, pom files

---
+++ 0.0.1/parrot-component/component-domain/src/test/java/com/parrot/eshop/domain/FakeTest.java
@@ -0,0 +1,23 @@
+/***************************************************************************************************
+ * Copyright 2007 NetCom. All rights reserved.
+ **************************************************************************************************/
+
+package com.parrot.eshop.domain;
+
+import org.junit.Test;
+
+
+/**
+ * @author tajzivit
+ */
+public class FakeTest {
+    
+    /**
+     * Test method for {@link java.lang.Object#toString()}.
+     */
+    @Test
+    public void testToString() {
+        
+    }
+    
+}
