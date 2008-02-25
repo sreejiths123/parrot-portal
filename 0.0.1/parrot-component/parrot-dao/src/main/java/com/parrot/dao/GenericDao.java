a7145b7bd651fc5701d23ca207057ea05de0c844
petrmac <petrmac@9f38408c-3b41-0410-94e9-c98551d686dc>
2008-02-25 19:57:32 +0000

    Externalized dao (first commit).

---
+++ 0.0.1/parrot-component/parrot-dao/src/main/java/com/parrot/dao/GenericDao.java
@@ -0,0 +1,34 @@
+package com.parrot.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Generic DAO interface providing base CRUD operations.
+ * 
+ * @author macekpet
+ * @param <T>
+ * @param <PK>
+ */
+public interface GenericDao<T, PK extends Serializable> {
+    
+    /** Persist the newInstance object into database */
+    PK create(T newInstance);
+    
+    /** Remove an object from persistent storage in the database */
+    void delete(T persistentObject);
+    
+    /**
+     * @return list of the objects
+     */
+    List<T> list();
+    
+    /**
+     * Retrieve an object that was previously persisted to the database using the indicated id as
+     * primary key
+     */
+    T read(PK id);
+    
+    /** Save changes made to a persistent object. */
+    void update(T transientObject);
+}
