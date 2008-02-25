07ab2de1a35adf0e55591415b3ca538eb94cb50b
petrmac <petrmac@9f38408c-3b41-0410-94e9-c98551d686dc>
2008-02-25 20:28:54 +0000

    Changed dao implementation location in dependent projects.

--- 0.0.1/parrot-component/parrot-component-domain/src/main/java/com/parrot/portal/domain/user/dao/IModuleDao.java
+++ 0.0.1/parrot-component/parrot-component-domain/src/main/java/com/parrot/portal/domain/user/dao/IModuleDao.java
@@ -1,6 +1,6 @@
 package com.parrot.portal.domain.user.dao;
 
-import com.parrot.portal.domain.dao.GenericDao;
+import com.parrot.dao.GenericDao;
 import com.parrot.portal.domain.user.IModule;
 
 public interface IModuleDao extends GenericDao<IModule, Integer> {
